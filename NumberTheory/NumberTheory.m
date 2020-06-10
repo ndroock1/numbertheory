f168028c15afe74f970f4eada6e5623266f145dd
Nilo <ndroock1@gmail.com>
2020-06-10 12:50:00 +0200

    Initial

---
+++ NumberTheory/NumberTheory.m
@@ -0,0 +1,16 @@
+(* Wolfram Language Package *)
+
+(* Created by the Wolfram Workbench 10-Jun-2020 *)
+
+BeginPackage["NumberTheory`"]
+(* Exported symbols added here with SymbolName::usage *) 
+
+Begin["`Private`"]
+(* Implementation of the package *)
+
+
+
+End[]
+
+EndPackage[]
+
