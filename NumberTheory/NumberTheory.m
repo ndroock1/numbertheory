8ce9e12e7f63796a7a497ade1ec1f251528ffc77
Nilo <ndroock1@gmail.com>
2020-06-14 21:07:34 +0200

    n2Q, q2N

--- NumberTheory/NumberTheory.m
+++ NumberTheory/NumberTheory.m
@@ -32,6 +32,17 @@ jordanTotient[n_Integer?Positive, k_ : 1] :=
 
 
 (* Exported symbols added here with SymbolName::usage *) 
+nNaturalToInteger::usage=
+			"nNaturalToInteger[n] maps N to Z"
+
+nIntegerToNatural::usage=
+			"nIntegerToNatural[n] maps Z to N"
+
+nNaturalToQuotient::usage=
+			"nNaturalToQuotient[n] maps N to Q"
+
+nQuotientToNatural::usage=
+			"nQuotientToNatural[n] maps Q to N"
 
 nCollatz::usage = 
 			"nCollatz[n] gives a list of the iterates in the 3n+1 problem,
@@ -50,12 +61,30 @@ nEulerPhi::usage =
 
 Begin["`Private`"]
 (* Implementation of the package *)
+nz[n_] := (-1)^(n - 1) Floor[n/2]
+zn[n_] := Piecewise[{{2 Abs[n], n < 0}, {2 n + 1, n > 0}, {1, n == 0}}]
+zq1[n_] := 
+ Piecewise[{{Apply[Times, 
+     Map[#[[1]]^nz[#[[2]] + 1] &, FactorInteger[n]]], n != 0}, {0, 
+    n == 0}}]
+zq[n_] := Piecewise[{{zq1[n], n > 0}, {-zq1[-n], n < 0}, {0, n == 0}}]
+qz1[n_] := 
+ Piecewise[{{Apply[Times, 
+     Map[#[[1]]^(zn[#[[2]]] - 1) &, FactorInteger[n]]], n != 0}, {0, 
+    n == 0}}]
+qz[n_] := Piecewise[{{qz1[n], n > 0}, {-qz1[-n], n < 0}, {0, n == 0}}]
+q2N[n_] := zn[qz[n]]
+n2Q[n_] := zq[nz[n]]
+nNaturalToInteger[n_] := nz[n]
+nIntegerToNatural[n_] := zn[n]
+nNaturalToQuotient[n_]:= n2Q[n]
+nQuotientToNatural[n_]:= q2N[n]
 
 nCollatz[1] := {1}
 nCollatz[n_Integer]  := Prepend[nCollatz[3 n + 1], n] /; OddQ[n] && n > 0
 nCollatz[n_Integer] := Prepend[nCollatz[n/2], n] /; EvenQ[n] && n > 0
 
-nFaulhaber[m_, n_] := Simplify[1/(m + 1) (BernoulliB[m + 1, n + 1] - BernoulliB[m + 1, 1])]
+nFaulhaber[m_Integer, n_Integer] := Simplify[1/(m + 1) (BernoulliB[m + 1, n + 1] - BernoulliB[m + 1, 1])]
 
 nEulerPhi[k_Integer, n_Integer] := DirichletConvolve[faulhaber[k, j], MoebiusMu[j] j^k, j, n]
 nEulerPhi[0, n] := EulerPhi[n]
